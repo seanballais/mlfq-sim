392547635e4ce5440f9022d8a579061ff5844864
Sean Francis N. Ballais <sean@seanballais.com>
2018-04-05 15:15:37 +0800

    Completed process control block.

--- src/mlfq_sim/ds/pcb.py
+++ src/mlfq_sim/ds/pcb.py
@@ -8,7 +8,15 @@ class ProcessControlBlock:
             self.length = length
 
         def __repr__(self):
-            return 'Execution History Item (executed starting at {0} for {1} units)'.format
+            return 'Execution History Item ' \
+                   '(executed starting at {0} for {1} units)'.format(self.start_time,
+                                                                     self.length)
+
+        def get_start(self):
+            return self.start_time
+
+        def get_length(self):
+            return self.length
 
     def __init__(self, pid, arrival_time, burst_time, priority):
         self.pid = pid
@@ -25,23 +33,24 @@ class ProcessControlBlock:
                                                                 self.burst_time,
                                                                 self.priority)
 
-    def get_pid():
+    def get_pid(self):
         return self.pid
 
-    def get_arrival_time():
+    def get_arrival_time(self):
         return self.arrival_time
 
-    def get_burst_time():
+    def get_burst_time(self):
         return self.burst_time
 
-    def get_remaining_time():
+    def get_remaining_time(self):
         return self.remaining_time
 
     def get_priority():
         return self.priority
 
-    def execute(start_time, length):
+    def execute(self, start_time, length):
         if self.remaining_time == 0:
             return
 
-        
+        self.remaining_time = max(self.remaining_time - length, 0)
+        self.execution_history.append(ExecutionHistoryItem(start_time, length))
