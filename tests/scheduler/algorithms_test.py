9993b8175bb7f92b8eb3dccbb35c34b27cb3dc8e
Sean Francis N. Ballais <sean@seanballais.com>
2018-05-26 15:28:43 +0800

    Added non-preemptive test.

--- tests/scheduler/algorithms_test.py
+++ tests/scheduler/algorithms_test.py
@@ -219,15 +219,83 @@ class TestSchedulingAlgorithms:
 
     def test_non_preemptive(self):
         # Test for case where there is no overlapping arrival times.
-        # Test for case where there is one overlapping arrival times.
+        processes = [ProcessControlBlock(0, 0, 5, 0),
+                     ProcessControlBlock(1, 5, 5, 0)]
+        assert algorithms.non_preemptive(processes) == ([0, 1], [], [], [], [], 10)
+
+        # Test for case where there are overlapping arrival times.
+        processes = [ProcessControlBlock(0, 0, 5, 0),
+                     ProcessControlBlock(1, 1, 3, 1)]
+        assert algorithms.non_preemptive(processes) == ([0, 1], [], [], [], [], 8)
+
         # Test for case where there are same arrival times for two processes but no running process.
+        processes = [ProcessControlBlock(0, 0, 5, 0),
+                     ProcessControlBlock(1, 0, 3, 1)]
+        assert algorithms.non_preemptive(processes) == ([1, 0], [], [], [], [], 8)
+
         # Test for case where there are two processes that arrived at the same time while another process is running.
+        processes = [ProcessControlBlock(0, 0, 5, 0),
+                     ProcessControlBlock(1, 1, 5, 1),
+                     ProcessControlBlock(2, 1, 5, 1)]
+        assert algorithms.non_preemptive(processes) == ([0, 1, 2], [], [], [], [], 15)
+
+        processes = [ProcessControlBlock(0, 0, 5, 0),
+                     ProcessControlBlock(1, 2, 2, 0),
+                     ProcessControlBlock(2, 2, 1, 1)]
+        assert algorithms.non_preemptive(processes) == ([0, 2, 1], [], [], [], [], 8)
+
         # Test for case where there is a gap between process arrivals.
+        processes = [ProcessControlBlock(0, 0, 5, 0),
+                     ProcessControlBlock(1, 10, 5, 1)]
+        assert algorithms.non_preemptive(processes) == ([0, 1], [], [], [], [], 15)
+
+        processes = [ProcessControlBlock(0, 0, 5, 0),
+                     ProcessControlBlock(1, 10, 5, 0)]
+        assert algorithms.non_preemptive(processes) == ([0, 1], [], [], [], [], 15)
+
         # Test for case where queue is preempted because the time slot was already filled up.
+        processes = [ProcessControlBlock(0, 0, 5, 0)]
+        resulting_process = ProcessControlBlock(0, 0, 5, 0)
+        resulting_process.execute(0, 2)
+        assert algorithms.non_preemptive(processes, time_allotment=2) == ([0], [], [], [resulting_process], [], 2)
+
         # Test for case where queue is preempted even though there are still processes arriving.
+        processes = [ProcessControlBlock(0, 10, 5, 0)]
+        assert algorithms.non_preemptive(processes, time_allotment=5) == ([], [ProcessControlBlock(0, 10, 5, 0)],
+                                                                          [], [], [], 5)
+
         # Test for case where queue is preempted even though there are still processes waiting to have a first run.
+        processes = [ProcessControlBlock(0, 0, 5, 0),
+                     ProcessControlBlock(1, 1, 5, 0)]
+        promoted_process = ProcessControlBlock(0, 0, 5, 0)
+        promoted_process.execute(0, 2)
+        assert algorithms.non_preemptive(processes, time_allotment=2) == ([0],
+                                                                          [],
+                                                                          [ProcessControlBlock(1, 1, 5, 0)],
+                                                                          [promoted_process],
+                                                                          [],
+                                                                          2)
+
+        processes = [ProcessControlBlock(0, 0, 5, 0),
+                     ProcessControlBlock(1, 0, 3, 1)]
+        promoted_process = ProcessControlBlock(1, 0, 3, 1)
+        promoted_process.execute(0, 2)
+        assert algorithms.non_preemptive(processes, time_allotment=2) == ([1],
+                                                                          [],
+                                                                          [ProcessControlBlock(0, 0, 5, 0)],
+                                                                          [promoted_process],
+                                                                          [],
+                                                                          2)
+
         # Test for case where another set of processes are added from another queue.
-        pass
+        processes = [ProcessControlBlock(0, 2, 5, 0)]
+        additional_processes = [ProcessControlBlock(1, 1, 5, 0)]
+        # New processes that have arrived already will tend to be the first ones to be executed.
+        assert algorithms.non_preemptive(processes, additional_processes, start_time=1) == ([1, 0], [], [], [], [], 11)
+
+        processes = [ProcessControlBlock(0, 0, 5, 0)]
+        additional_processes = [ProcessControlBlock(1, 0, 3, 1)]
+        assert algorithms.non_preemptive(processes, additional_processes) == ([1, 0], [], [], [], [], 8)
 
     def test_preemptive(self):
         # Test for case where there is no overlapping arrival times.
