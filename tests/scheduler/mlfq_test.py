3dcfef69fe037e23fb617303d7febeda6f667172
Sean Francis N. Ballais <sean@seanballais.com>
2018-05-29 09:05:22 +0800

    Added additional tests.

--- tests/scheduler/mlfq_test.py
+++ tests/scheduler/mlfq_test.py
@@ -153,6 +153,21 @@ class TestMLFQ:
         assert process[1].get_pid() == 1
         assert run_time == 15
 
+        # Invoke processes to remain in the same queue.
+        processes = [ProcessControlBlock(0, 0, 10, 0),
+                     ProcessControlBlock(1, 0, 10, 0),
+                     ProcessControlBlock(2, 10, 10, 0)]
+        queues = [mlfq.MLFQQueue(algorithms.round_robin, quanta=5),
+                  mlfq.MLFQQueue(algorithms.round_robin, quanta=5)]
+
+        mlfq1 = mlfq.MLFQ(processes, queues)
+        schedule, process, run_time = mlfq1.simulate()
+        assert schedule == [0, 1, 2, 0, 1, 2]
+        assert process[0].get_pid() == 0
+        assert process[1].get_pid() == 1
+        assert process[2].get_pid() == 2
+        assert run_time == 30
+
     def test_three_queues(self):
         # SCENARIO: Testing higher over lower priority MLFQ scheduling.
         # Test with one process only.
@@ -243,3 +258,11 @@ class TestMLFQ:
         assert process[0].get_pid() == 0
         assert process[1].get_pid() == 1
         assert run_time == 15
+
+    def test_nearest_preemption_time(self):
+        mlfq1 = mlfq.MLFQ([], [])
+        assert mlfq1._nearest_preemption_time(1) is None
+
+        mlfq1 = mlfq.MLFQ([ProcessControlBlock(0, 0, 1, 0),
+                           ProcessControlBlock(1, 5, 3, 0)], [])
+        assert mlfq1._nearest_preemption_time(1) == 5
