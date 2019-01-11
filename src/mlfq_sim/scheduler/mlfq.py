c2a2617bcb8c5e75b985ef47810ecab80ad9ec23
Sean Francis N. Ballais <sean@seanballais.com>
2019-01-11 11:18:35 +0800

    Fixed bug where context switched processes are no longer scheduled again in a single queue MLFQ configuration.

--- src/mlfq_sim/scheduler/mlfq.py
+++ src/mlfq_sim/scheduler/mlfq.py
@@ -91,66 +91,64 @@ class MLFQ:
             while not self.arrival_queue.empty():
                 self.queues[0].add_process(self.arrival_queue.get())
 
-            schedule, _, _, _, _, run_time = self.queues[0].execute(0, 0)
-        else:
-            while not self.arrival_queue.empty() or self._queues_has_processes():
-                preemption_time = self._nearest_preemption_time(run_time)
+        while not self.arrival_queue.empty() or self._queues_has_processes():
+            preemption_time = self._nearest_preemption_time(run_time)
+            current_queue = self.queues[current_queue_index]
+
+            if current_queue_index == 0:
+                while not self.arrival_queue.empty():
+                    current_queue.add_process(self.arrival_queue.get())
+
+            traversed_queues = 0
+            found_queue_with_processes = True
+            while not current_queue.has_processes():
+                current_queue_index = (current_queue_index + 1) % len(self.queues)
                 current_queue = self.queues[current_queue_index]
+                traversed_queues += 1
 
-                if current_queue_index == 0:
-                    while not self.arrival_queue.empty():
-                        current_queue.add_process(self.arrival_queue.get())
+                if traversed_queues == len(self.queues):
+                    found_queue_with_processes = False
+                    break
 
-                traversed_queues = 0
-                found_queue_with_processes = True
-                while not current_queue.has_processes():
-                    current_queue_index = (current_queue_index + 1) % len(self.queues)
-                    current_queue = self.queues[current_queue_index]
-                    traversed_queues += 1
+            if not found_queue_with_processes:
+                run_time = preemption_time
+                current_queue_index = 0
+                continue
 
-                    if traversed_queues == len(self.queues):
-                        found_queue_with_processes = False
-                        break
+            (queue_schedule,
+             arrival_queue,
+             wait_queue,
+             promoted_processes,
+             demoted_processes,
+             run_time
+             ) = current_queue.execute(time_allotment, run_time)
 
-                if not found_queue_with_processes:
-                    run_time = preemption_time
-                    current_queue_index = 0
-                    continue
+            schedule += queue_schedule
 
-                (queue_schedule,
-                 arrival_queue,
-                 wait_queue,
-                 promoted_processes,
-                 demoted_processes,
-                 run_time
-                 ) = current_queue.execute(time_allotment, run_time)
+            for process in arrival_queue:
+                self.arrival_queue.put(process)
 
-                schedule += queue_schedule
+            for process in wait_queue:
+                current_queue.add_process(process)
 
-                for process in arrival_queue:
-                    self.arrival_queue.put(process)
+            for process in promoted_processes:
+                self.queues[max(0, current_queue_index - 1)].add_process(process)
 
-                for process in wait_queue:
-                    current_queue.add_process(process)
+            for process in demoted_processes:
+                self.queues[min(len(self.queues) - 1, current_queue_index + 1)].add_process(process)
 
-                for process in promoted_processes:
-                    self.queues[max(0, current_queue_index - 1)].add_process(process)
-
-                for process in demoted_processes:
-                    self.queues[min(len(self.queues) - 1, current_queue_index + 1)].add_process(process)
-
-                if preemption_time is not None:
-                    if preemption_time - run_time > 0:
-                        # Hoooo boi, we got space to move to the next queue.
-                        # No need to worry about the time in the next queues.
-                        current_queue_index = min(len(self.queues) - 1, current_queue_index + 1)
-                        time_allotment = preemption_time - run_time
-                    else:
-                        # We gotta go back to the top!
-                        current_queue_index = 0
-                        time_allotment = 0
+            if preemption_time is not None:
+                if preemption_time - run_time > 0:
+                    # Hoooo boi, we got space to move to the next queue.
+                    # No need to worry about the time in the next queues.
+                    current_queue_index = min(len(self.queues) - 1, current_queue_index + 1)
+                    time_allotment = preemption_time - run_time
                 else:
+                    # We gotta go back to the top!
+                    current_queue_index = 0
                     time_allotment = 0
+            else:
+               time_allotment = 0
 
         return self._merge_same_adjacent_elements(schedule), self.processes, run_time
 
